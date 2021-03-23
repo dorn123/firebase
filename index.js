8a91bfbf653d2a1eb37fb6d1c2f755618b3a09a2
dorn123 <36752565+dorn123@users.noreply.github.com>
2021-03-23 22:33:08 +0700

    update

---
+++ index.js
@@ -0,0 +1,36 @@
+let userList = document.querySelector('#userList');
+let form = document.querySelector('#addUser');
+
+function renderUser(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let surname = document.createElement('span');
+    let city = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    surname.textContent = doc.data().surname;
+    city.textContent = doc.data().city;
+
+    li.appendChild(name);
+    li.appendChild(surname);
+    li.appendChild(city);
+    userList.appendChild(li);
+}
+
+db.collection('users').get().then(user =>{
+    user.docs.forEach(doc => {
+        console.log(doc.data());
+        renderUser(doc);
+    })
+});
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    db.collection('users').add({
+        name: form.name.value,
+        surname: form.surname.value,
+        city: form.city.value
+    })
+
+})
